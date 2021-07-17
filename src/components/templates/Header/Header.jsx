e7741c84a7d28ba47220a2ddddc165556e622aed
dungnc <dung.dev.gramer@gmail.com>
2021-07-18 05:30:43 +0700

    💄 Done Header

--- src/components/templates/Header/Header.jsx
+++ src/components/templates/Header/Header.jsx
@@ -2,32 +2,35 @@ import styles from './Header.module';
 import PropTypes from 'prop-types';
 import Logo from '@public/assets/images/logo.svg';
 import { Link, NavLink } from 'react-router-dom';
-
-const NavLinks = () => {
-  const paths = [
-    {
-      location: '/components',
-      name: 'Components',
-    },
-    {
-      location: '/blog',
-      name: 'Blog',
-    },
-  ];
-  return (
-    <ul>
-      {paths.map(path => (
-        <li key={path.name}>
-          <NavLink to={path.location} activeClassName={styles.active}>
-            {path.name}
-          </NavLink>
-        </li>
-      ))}
-    </ul>
-  );
-};
+import { useState } from 'react';
 
 function Header(props) {
+  const [open, setOpen] = useState(false);
+
+  const NavLinks = () => {
+    const paths = [
+      {
+        location: '/components',
+        name: 'Components',
+      },
+      {
+        location: '/blog',
+        name: 'Blog',
+      },
+    ];
+    return (
+      <ul className={`${styles['nav-list']} ${open ? styles.open : ''}`}>
+        {paths.map(path => (
+          <li key={path.name}>
+            <NavLink to={path.location} activeClassName={styles.active}>
+              {path.name}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
@@ -35,12 +38,15 @@ function Header(props) {
           <Link to="/">
             <Logo width="50" height="50" />
           </Link>
+          <i
+            className={`${styles.menu} far fa-bars`}
+            onClick={() => setOpen(!open)}
+          />
         </div>
 
-        <nav className={styles['nav-links']}>
+        <nav className={styles['nav-container']}>
           <NavLinks />
         </nav>
-        <i className="fal fa-bars"></i>
       </div>
     </header>
   );
