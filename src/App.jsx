57df3fd05674f0454535b2468394a3530b0291cd
dungnc <dung.dev.gramer@gmail.com>
2021-07-29 03:56:08 +0700

    ✅ Transition fade when change page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import 'App.scss';
 
-import { Switch } from 'react-router-dom';
+import { Switch, useLocation } from 'react-router-dom';
 
 import Routes from './routes';
 import { Sidebar } from '@organisms/Sidebar/Sidebar';
 import Header from '@organisms/Header/Header';
 
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { useRef } from 'react';
+
 function App() {
+  const location = useLocation();
+  let nodeRef = useRef(null);
   return (
     <>
       <Header />
-      <main className='main-container'>
+      <main className="main-container">
         <Sidebar />
-        <section className='main'>{Routes}</section>
+        <section className="main">
+          <TransitionGroup appear>
+            <CSSTransition
+              unmountOnExit
+              key={location.key}
+              classNames="fade"
+              timeout={{ enter: 250, exit: 0 }}
+            >
+              <Switch location={location}>
+                {Routes}
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        </section>
       </main>
     </>
   );
