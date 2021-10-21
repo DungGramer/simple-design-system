619a6860042f7fdb37fcf7bc5ffd513f08082a4d
dungnc <dung.dev.gramer@gmail.com>
2021-10-21 14:04:57 +0700

    🚀 Update package

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,18 +1,11 @@
 const autoprefixer = require('autoprefixer');
 const flexBugsFixes = require('postcss-flexbugs-fixes');
-const presetEnv = require('postcss-preset-env');
 const postcssNormalize = require('postcss-normalize');
 
 module.exports = {
   plugins: [
     autoprefixer({ grid: 'autoplace' }),
     flexBugsFixes(),
-    presetEnv({
-      autoprefixer: {
-        flexbox: 'no-2009',
-      },
-      stage: 3,
-    }),
     postcssNormalize(),
   ],
 };
